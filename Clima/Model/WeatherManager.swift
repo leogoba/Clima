86be40b7f9ea2a000198cc2b09236dae2b514c17
leogoba <leogobamusic@yandex.ru>
2023-02-18 22:25:36 +0400

    Work before JSON request

---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by leogoba on 18.02.2023.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=64d5291ec4a3112760625f5e20aece12&units=metric"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        print(urlString)
+    }
+}
